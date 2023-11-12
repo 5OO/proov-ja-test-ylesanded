82caaf8e337195583cc09cf4db9f20ec3a8e3fb7
5OO <git.hub@myrk.eu>
2023-11-12 12:49:16 +0200

    ülesanne lahendatud

--- src/main/java/org/hyperskill/Main.java
+++ src/main/java/org/hyperskill/Main.java
@@ -8,5 +8,19 @@ public class Main {
     public static void main(String[] args) {
                 Scanner scanner = new Scanner(System.in);
                 // start coding here
+        System.out.println("Enter numbers (1 to stop):");
+        int input=0;
+        while (input !=1) {
+            input = scanner.nextInt();
+            if (input % 2 == 0) {
+                System.out.println("The number is even.  " + input/2);
+
+            } else {
+                System.out.println("The number is odd.  " + input * 3 + 1);
+
+            }
+        }
+
+
     }
 }
